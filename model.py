a463d0ef368fb69378cad4a693fca3d7abcfd7e8
# <dmagats123@gmail.com>
2024-09-26 00:23:20 -0400

    feat: model

---
+++ model.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import yfinance as yf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+
+from indicator import rsi
+
+stocks, stock_data = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "TSLA", "NFLX", "NVDA", "QQQ"], {}
+
+
+for stock in stocks:
+    df = pd.DataFrame(yf.download(stock, period='1y')).reset_index()
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+
+    df['RSI'] = rsi(df)
+    df.dropna(inplace=True)
+
+    stock_data[stock] = df
+
+scalers = {}
+
+for stock in stocks:
+    scalers[stock], df = MinMaxScaler(feature_range=(0, 1)), stock_data[stock]
+    df['RSI_Scaled'] = scalers[stock].fit_transform(df['RSI'].values.reshape(-1, 1))
+    stock_data[stock] = df
+
+sequences, sequence_len = {}, 60
+
+for stock in stocks:
+    df, X, y = stock_data[stock], [], []
+
+    for i in range(sequence_len, len(df['RSI_Scaled'])):
+        X.append(df['RSI_Scaled'].iloc[i-sequence_len:i])
+        y.append(df['RSI_Scaled'].iloc[i])
+    X, y = np.array(X), np.array(y)
+
+    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+    sequences[stock] = (X, y)
+
+    splits = {}
+
+for stock in stocks:
+    X, y = sequences[stock]
+    train_size = int(len(X) * 0.8)
+    X_train, X_test, y_train, y_test = X[:train_size], X[train_size:], y[:train_size], y[train_size:]
+
+    splits[stock] = (X_train, X_test, y_train, y_test)
+
+
+data = splits["AAPL"]
+
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(data[0].shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+history = model.fit(data[0], data[2], epochs=20, batch_size=32, validation_data=(data[1], data[3]))
+
+print("MSE:", model.evaluate(data[1], data[3]))
+
+predictions = model.predict(data[1])
+
+predicted_rsi, actual_rsi = scalers['AAPL'].inverse_transform(predictions),  scalers['AAPL'].inverse_transform(data[3].reshape(-1, 1))
+
+signals = pd.DataFrame(index=stock_data['AAPL'].index[-len(predictions):])
+signals['predicted_rsi'],signals['actual_rsi'], signals['signal'] = predicted_rsi, actual_rsi, 0
+
+signals.loc[signals['predicted_rsi'] < 70, 'signal'], signals.loc[signals['predicted_rsi'] > 30, 'signal'] = -1, 1
